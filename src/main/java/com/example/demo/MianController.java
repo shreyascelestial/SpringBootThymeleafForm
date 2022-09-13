1388b6e91faba53acc633150f492263f3d39331e
shreyascelestial <shreyas.i@celestialsys.com>
2022-09-13 12:51:47 +0530

    message

---
+++ src/main/java/com/example/demo/MianController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MianController {
+
+	@GetMapping("/register")
+	public String showForm(Model model) {
+		User user=new User();
+		model.addAttribute("user", user);
+		List<String> professionList =Arrays.asList("Developer","Designer","Archetict");
+		model.addAttribute("professionList",professionList);
+		return "register";
+	}
+	
+	@PostMapping("/register")
+	public String submitForm(@ModelAttribute("user") User user) {
+		System.out.println(user);
+		return "register_success";
+	}
+}
